ca6776e0a0d468a8841ba913990be5e60fc3c82e
Ferrian Septiawan <ferrian27@gmail.com>
2020-12-13 16:30:05 +0700

    add delete user

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('admin.dashboard');
+    return view('welcome');
 });
 
 Route::prefix('admin')
